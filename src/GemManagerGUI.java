821308bb995e798831ee15aaeae22cc6cf70addd
Jagat060 <jagatsubedi1010@gmail.com>
2025-06-14 22:00:03 -0600

    matches are checked and board updated in GUI

--- src/GemManagerGUI.java
+++ src/GemManagerGUI.java
@@ -1,21 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
-public class GemManagerGUI extends Board{
+public class GemManagerGUI extends Board {
     JFrame frame = new JFrame("Gem Manager Game");
     JPanel panel1 = new JPanel();
     JPanel panel2 = new JPanel();
     JButton[][] buttons;
-    private java.awt.event.ActionListener ActionListener;
 
-    GemManagerGUI(int w, int h){
+    GemManagerGUI(int w, int h) {
         super(w, h);
         buttons = new JButton[w][h];
         makeFrame();
     }
-    public void makeFrame(){
-        frame.setSize(800,800);
+
+    public void makeFrame() {
+        frame.setSize(800, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
@@ -25,14 +24,14 @@ public class GemManagerGUI extends Board{
         panel2.setPreferredSize(new Dimension(200, 800));
         panel2.setBackground(Color.white);
 
-        for(int i=0; i<height; i++) {
+        for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 JButton button = new JButton();
                 //button.setOpaque(true);  //if needed background color in buttons.
                 buttons[i][j] = button;
                 buttonDesign(i, j, button);
                 panel1.add(button);
-                button.addActionListener(ActionListener);
+                //button.addActionListener(ActionListener);
             }
         }
 
@@ -44,7 +43,7 @@ public class GemManagerGUI extends Board{
 
     }
 
-    public void buttonDesign(int row, int col, JButton button){
+    public void buttonDesign(int row, int col, JButton button) {
         int w = 80;
         int h = 80;
         ImageIcon Icon1 = new ImageIcon("gemstone_6057422.png");
@@ -61,24 +60,27 @@ public class GemManagerGUI extends Board{
         ImageIcon icon2 = new ImageIcon(ICon2);
         ImageIcon icon3 = new ImageIcon(ICon3);
         ImageIcon icon4 = new ImageIcon(ICon4);
+//        icon(row, col, button, icon1, icon2, icon3, icon4);
 
-        if(board[row][col] == '*'){
+        if (board[row][col] == '*') {
             button.setIcon(icon1);
             button.setBackground(new Color(128, 0, 128));
         }
-        if(board[row][col] == '+'){
+        if (board[row][col] == '+') {
             button.setIcon(icon2);
             button.setBackground(Color.RED);
         }
-        if(board[row][col] == '#'){
+        if (board[row][col] == '#') {
             button.setIcon(icon3);
             button.setBackground(Color.yellow);
         }
-        if(board[row][col] == '%'){
+        if (board[row][col] == '%') {
             button.setIcon(icon4);
             button.setBackground(Color.ORANGE);
         }
-
-
+        checkMatch();
     }
+
+
+
 }
